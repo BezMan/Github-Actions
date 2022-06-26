7e566f7c7bfc48f2e25ba5310fd211fc0b442c19
betzalels <betzalels@earnix.com>
2022-06-26 15:59:57 +0300

    init

---
+++ app/src/main/java/com/example/mycompose/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mycompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DefaultPreview()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Column {
+        Text("Love $name!")
+        Text("Go $name!")
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Greeting("Android")
+}
