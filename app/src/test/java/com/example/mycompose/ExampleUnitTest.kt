27696718b53b7216b364d16c22fc255f772015e4
betzalels <betzalels@earnix.com>
2022-06-27 14:54:11 +0300

    generate apk

--- app/src/test/java/com/example/mycompose/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mycompose/ExampleUnitTest.kt
@@ -14,6 +14,11 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun mult_isCorrect() {
+        assertEquals(4, 2 * 2)
+    }
 //
 //    @Test
 //    fun failing_test() {
