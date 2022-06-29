bef4f4cb2cdb2ae8fa1acb8252bc1b02a677de42
betzalels <betzalels@earnix.com>
2022-06-29 11:00:45 +0300

    try to add commit and push

--- test.sh
+++ test.sh
@@ -4,10 +4,9 @@ echo  "--------  Compiling...  --------"
 echo  "--------  Compiling...  --------"
 echo  "--------  Compiling...  --------"
 echo  "--------  Compiling...  --------"
-echo  "--------  Compiling...  --------"
-echo  "--------  Compiling...  --------"
-echo  "--------  Compiling...  --------"
-echo  "--------  Compiling...  --------"
+
 
 cp test.sh test_copy.txt
 echo "$(<test_copy.txt )"
+
+echo 'text hereeeeee' >> test.sh
