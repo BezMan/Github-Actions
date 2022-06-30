4939bda8632d734cd133b2062e4e6afd8d5e19dc
betzalels <betzalels@earnix.com>
2022-06-30 10:26:25 +0300

    unit tests using Github Actions directly and using script

--- app/src/test/java/com/example/mycompose/ExampleUnitTest.kt
+++ app/src/test/java/com/example/mycompose/ExampleUnitTest.kt
@@ -9,32 +9,279 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun mult_isCorrect2() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect3() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect4() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect5() {
+        assertEquals(4, 2 * 2)
     }
 
     @Test
     fun mult_isCorrect() {
         assertEquals(4, 2 * 2)
     }
-//
+
+    @Test
+    fun mult_isCorrect6() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect7() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect8() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect9() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect10() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect11() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect12() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect13() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect1() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect14() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect15() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect16() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect17() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect18() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect19() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect20() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect21() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect22() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect23() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect24() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect25() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect26() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect27() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect28() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect29() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect30() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect31() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect32() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect33() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect34() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect35() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect36() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect37() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect38() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect39() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect40() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect41() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect42() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect43() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect44() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect45() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect46() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect47() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect48() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect49() {
+        assertEquals(4, 2 * 2)
+    }
+
+    @Test
+    fun mult_isCorrect50() {
+        assertEquals(4, 2 * 2)
+    }
+
+
 //    @Test
 //    fun failing_test() {
 //        fail("on purpose")
 //    }
 
-    @Test
-    fun loop_20_sec() {
-        for (i in 1..20) {
-            check_equal(i)
-            Thread.sleep(1000)
-        }
-    }
-
-    private fun check_equal(i: Int) {
-        assertEquals(i, i)
-    }
+//    @Test
+//    fun loop_20_sec() {
+//        for (i in 1..20) {
+//            check_equal(i)
+//            Thread.sleep(1000)
+//        }
+//    }
+//
+//    private fun check_equal(i: Int) {
+//        assertEquals(i, i)
+//    }
 
 
 }
